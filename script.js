f156396013f78344dd36ba014d2f929d8c18a160
israelshittu430-web <israelshittu430@gmail.com>
2025-11-07 03:25:45 -0800

    Second commit

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// DOM until DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function(){
+// Wait until the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function (){
 
-    //Det DOM elements
-    const hamburger = document.querySelector(".hambugger");
-    const navMenu = document.querySelector(".nav-menu");
+    // Get DOM elements
+    const hamburger = document.querySelector('.hamburgger');
+    const navMenu = document.querySelector('.nav-menu');
 
-    // Toggle menu when hamburger is clicked
-    hamburger.addEventListener("click", function(){
-        navMenu.classList.toggle("active")
-        hamburger.classList.toggle("active");
+    //Toggle menu when hamburger is clicked
+    hamburger.addEventListener('click', function (){
+        navMenu.classList.toggle('active');
+        hamburger.classList.toggle('active');
     });
-    // Handle window resize: auto-close mobile menu on desktop view
-    window.addEventListener("resize", function(){
-        if(this.window.innerWidth > 768){
-            // on desktop, ensure menu is visible and hamburger is great
-            navMenu.classList.remove("active")
-            hamburger.classList.remove("active")
-        };
-    })
-})
 
+    // Handle wimdow resize: auto-close mobile menu on desktop view
+    window.addEventListener('resize', function(){
+        if (window.innerWidth > 768) {
+            // On desktop, ensure menu is visible and hambugger is reset
+            navMenu.classList.remove('active')
+            hamburger.classList.remove('active')
+        };
+    });
+});
